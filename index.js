ad7e75c570e7b0fee562ca236cf37bfcd35cbe7c
danLiu <dan.liu@crimsoneducation.org>
2025-01-27 18:07:25 +0800

    fix: test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log({
 window.bridge = appBridge.window.StoryblokBridge;
   const btn1 = document.getElementById("btn1");
 
-  appBridge.window.StoryblokBridge.on(["input", "change"], (event) => {
+  appBridge.window.StoryblokBridge.on('input', (event) => {
     console.log("实时监听到用户操作:", event);
   });
 });
