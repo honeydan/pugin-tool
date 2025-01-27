5a228e5208706373f191952f3fbfb66cb2a84675
danLiu <dan.liu@crimsoneducation.org>
2025-01-27 17:22:54 +0800

    feat: test

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // const StoryblokClient = require("storyblok-js-client");
 const appBridge = new window.Storyblok.bridge();
+const btn1 = document.getElementById('#btn1');
+btn1.addEventListener('click', () => {
+    appBridge.getCurrentStory().then((story) => {
+        console.log("当前正在编辑的 story:", story);
+      });
+});
 
 appBridge.on(["input", "change"], (event) => {
   console.log("实时监听到用户操作:", event);
 });
 
-appBridge.getCurrentStory().then((story) => {
-  console.log("当前正在编辑的 story:", story);
-});
 
-getAllStories().then(() => {
-  console.log("Finished");
-});
+
+
