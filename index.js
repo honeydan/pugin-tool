cd3a9d1e523c757350eff73396f7f28060eb3839
danLiu <dan.liu@crimsoneducation.org>
2025-01-27 18:31:43 +0800

    fix

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const appBridge = new StoryblokBridge({
-        accessToken: "m5HATAyyFBzgZQgnBr6lgQtt",
+  const appBridge = new StoryblokBridge({
+    accessToken: "m5HATAyyFBzgZQgnBr6lgQtt",
+  });
+  if (appBridge.isInEditor()) {
+    console.log(window);
+    window.bridge = appBridge;
+    const inputlist = document.querySelectorAll("input");
+    console.log({
+      inputlist,
     });
-    if (appBridge.isInEditor()) {
-        console.log(window)
-        window.bridge = appBridge;
-          const btn1 = document.getElementById("btn1");
-          btn1.addEventListener('click', () => {
-            const inputlist = document.querySelectorAll('input');
-            console.log({
-                inputlist
-            })
-          })
-        
-          appBridge.on('change', (event) => {
-            console.log("实时监听到用户操作:", event);
-          });
-    }
-//   const appBridge = new window.Storyblok.bridge();
-
+    appBridge.on("change", (event) => {
+      console.log("实时监听到用户操作:", event);
+    });
+  }
+  //   const appBridge = new window.Storyblok.bridge();
 });
