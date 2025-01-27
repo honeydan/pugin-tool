4aa58ec6e04f0d57dcbe15e2517bc56b43448e90
danLiu <dan.liu@crimsoneducation.org>
2025-01-27 17:59:33 +0800

    fix: test

--- index.js
+++ index.js
@@ -9,18 +9,7 @@ console.log({
 })
   const btn1 = document.getElementById("btn1");
 
-  btn1.addEventListener("click", () => {
-    appBridge
-      .getCurrentStory()
-      .then((story) => {
-        console.log("当前正在编辑的 story:", story);
-      })
-      .catch((error) => {
-        console.error("获取 story 失败:", error);
-      });
-  });
-
-  appBridge.on(["input", "change"], (event) => {
+  appBridge.window.StoryblokBridge.on(["input", "change"], (event) => {
     console.log("实时监听到用户操作:", event);
   });
 });
